52e41a6df871338ebefecbfeb879b9330b78158a
JohanBoDev <bohorquezjohan958@gmail.com>
2024-10-04 19:01:03 -0500

    Dogma inicio

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './index.html',
+    './login.html',
+    './dashboard-usuario/*.html',
+    './css/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
